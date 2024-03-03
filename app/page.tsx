f3c7411508f2f7dc7df11cad51c5b04aceb8bf42
DanielDerma <danielderma.contact@gmail.com>
2024-03-03 16:14:53 -0600

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
 "use client";
-import { ChatButton } from "@/components/chatopensource";
+import { Chat } from "@/components/chatopensource";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import React from "react";
 
 export default function WavyBackgroundDemo() {
   return (
-    <>
-      <WavyBackground className="max-w-4xl mx-auto pb-40">
-        <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-          X-RayDoc Assistant
-        </p>
-        <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
-          Tools for medical professionals to help them with Chest X-rays for COVID-19.
-        </p>
-      </WavyBackground>
-      <ChatButton />
-    </>
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+        X-RayDoc Assistant
+      </p>
+      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+        Tools for medical professionals to help them with Chest X-rays for COVID-19.
+      </p>
+    </WavyBackground>
   );
 }
